72a7e8417a5fe53c69ff633d40636aaea2d6eb2d
Huy Quang <tranquanghuy288@gmail.com>
2018-07-09 16:59:23 +0700

    remove jar files

--- src/main/java/org/ucsf/glv/webapp/config/ServletConfig.java
+++ src/main/java/org/ucsf/glv/webapp/config/ServletConfig.java
@@ -7,7 +7,7 @@ import javax.ws.rs.core.Application;
  * class is used to declare jersey sevlet instead of xml config
  */
 
-@ApplicationPath("/*")
+@ApplicationPath("/")
 public class ServletConfig extends Application {
     
 }
